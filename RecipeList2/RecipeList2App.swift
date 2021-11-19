dd5c7b25760b03eea9e5ed68858f0f5834f9be67
Fredrik Pahlm <fredrik.pahlm@sensolution.se>
2021-11-19 10:14:45 -0800

    Add tabview.swift

--- RecipeList2/RecipeList2App.swift
+++ RecipeList2/RecipeList2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RecipeList2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipeTabView()
         }
     }
 }
--- RecipeList2/View Model/RecipeModel.swift
+++ RecipeList2/View Model/RecipeModel.swift
@@ -15,3 +15,4 @@ class RecipeModel: ObservableObject {
     }
 }
 
+// Extra
